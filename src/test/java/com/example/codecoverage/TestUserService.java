62e32f5f0d5babf06a21e2c3bf0aca16b5b752dc
saurabh <sourabh.sharma0185@gmail.com>
2022-02-12 22:04:52 +0530

    update the testcases for service.

--- src/test/java/com/example/codecoverage/TestUserService.java
+++ src/test/java/com/example/codecoverage/TestUserService.java
@@ -1,9 +1,41 @@
 package com.example.codecoverage;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.codecoverage.modal.User;
+import com.example.codecoverage.repository.UserRepository;
+import com.example.codecoverage.service.UserService;
 
 @ExtendWith(MockitoExtension.class)
 public class TestUserService {
+
+	@Mock
+    private UserRepository userRepository;
+
+    UserService userService;
+    
+    @BeforeEach
+    void initUseCase() {
+    	//userService = new UserService(userRepository);
+    }
+    
+    @Test
+    public void savedUser_Success() {
+        User user = new User();
+        user.setFirstName("sajedul");
+        user.setLastName("karim");
+
+        when(userRepository.save(any(User.class))).thenReturn(user);
+        User savedUser = userRepository.save(user);
+        assertThat(savedUser.getFirstName()).isNotNull();
+    }
 
 }
