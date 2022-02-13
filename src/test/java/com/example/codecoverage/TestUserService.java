49a0b3e096166a6f484a1ee968a0f6cdbaa28fdc
saurabh <sourabh.sharma0185@gmail.com>
2022-02-13 22:07:12 +0530

    update test cases for service .

--- src/test/java/com/example/codecoverage/TestUserService.java
+++ src/test/java/com/example/codecoverage/TestUserService.java
@@ -2,9 +2,13 @@ package com.example.codecoverage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.BeforeEach;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -24,10 +28,14 @@ public class TestUserService {
 	@InjectMocks
     UserService userService;
     
-    @BeforeEach
-    void initUseCase() {
-    	//userService = new UserService(userRepository);
-    }
+	@Test
+	public void listAllUser_Success(){
+		List<User> users = new ArrayList<User>();
+		users.add(new User(1,"saurabh","shamra"));
+		when(userRepository.findAll()).thenReturn(users);
+		assertEquals(users, userService.listAllUser());
+		assertEquals(1, userService.listAllUser().size());
+	}
     
     @Test
     public void savedUser_Success() {
@@ -38,6 +46,21 @@ public class TestUserService {
         when(userRepository.save(any(User.class))).thenReturn(user);
         User savedUser = userService.saveUser(user);
         assertEquals(user, savedUser);
+    }
+    
+    @Test
+    public void getUser_Success() {
+    	User user = new User(1,"saurabh","sharma");
+    	when(userRepository.findById(1)).thenReturn(Optional.of(user));
+    	User searchUser = userService.getUser(1);
+    	assertEquals(user, searchUser);
+    }
+    
+    @Test
+    public void deleteUser_Success() {
+    	Integer userId = 1;
+    	userService.deleteUser(userId);
+    	verify(userRepository,times(1)).deleteById(userId);
     }
 
 }
